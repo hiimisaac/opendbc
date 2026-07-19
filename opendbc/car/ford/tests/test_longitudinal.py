b9a9d0c8bd4314b523de3982bf7fdf7b805b0b81
Isaac Barham <development@isaacbarham.com>
2026-07-18 23:00:06 -0400

    Ford: restore smooth brake-to-gas transitions

--- opendbc/car/ford/tests/test_longitudinal.py
+++
@@ -1,7 +0,0 @@
-from opendbc.car.ford.carcontroller import apply_brake_gas_interlock
-from opendbc.car.ford.values import CarControllerParams
-
-
-def test_brake_request_never_sends_positive_propulsion():
-  assert apply_brake_gas_interlock(0.2, brake_request=True) == CarControllerParams.INACTIVE_GAS
-  assert apply_brake_gas_interlock(0.2, brake_request=False) == 0.2
